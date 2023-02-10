a808ec995bedc0b441353fd6dbf6879a549003ea
Pedro H M Ferreira <100206641+pedromferreira97@users.noreply.github.com>
2023-02-10 11:30:53 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { UserauthService } from './servico/userauth.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit{
-  tipoPerfil = "Usuário";
   public appPages = [
     { title: 'Início', url: '/inicio/inicio', icon: 'home' },
     { title: 'Perfil', url: '/perfil/perfil', icon: 'person' },
